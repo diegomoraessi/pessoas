97fe415bce3a1483ce892627b92b9d45b5ed842e
Diego Moraes <diegoo.fonsecaa@gmail.com>
2025-02-18 16:19:49 -0300

    Sofrendo

--- src/main/java/com/example/cpfsers/homecontroller/HomeController.java
+++ src/main/java/com/example/cpfsers/homecontroller/HomeController.java
@@ -3,11 +3,13 @@ package com.example.cpfsers.homecontroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.cpfsers.pessoa.Pessoa;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String showHomePage() {
+    public String showHomePage(Pessoa pessoa) {
         return "index";
     }
 
--- src/main/java/com/example/cpfsers/pessoa/PessoaController.java
+++ src/main/java/com/example/cpfsers/pessoa/PessoaController.java
@@ -2,21 +2,29 @@ package com.example.cpfsers.pessoa;
 
 import java.util.List;
 
+import javax.naming.Binding;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pessoa")
+@RequestMapping
 public class PessoaController {
 
     @Autowired
     public PessoaService pessoaService;
 
-    @GetMapping("/save")
-    public PessoaDTO save(PessoaDTO pessoaDTO) {
-        return pessoaService.save(pessoaDTO);
+    @PostMapping("/salvar")
+    public String save(Pessoa pessoa, Model model) {
+
+        pessoaService.save(new PessoaDTO(pessoa.getNome(), pessoa.getCpf(), pessoa.getIdade(), pessoa.getId()));
+        return "redirect:/pessoa/listar";
     }
 
     @GetMapping("/listar")
